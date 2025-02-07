55141a9eaf87db6ef2f15d61122c9e4b4762cbd3
oktaycabik <oktaycabik2@gmail.com>
2025-02-08 02:34:40 +0300

    Fix CIDR format in security group

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,7 +50,7 @@ function App() {
             placeholder="Yeni görev ekle..."
             className="todo-input"
           />
-          <button type="submit" className="add-button">EkleeEeeee</button>
+          <button type="submit" className="add-button">EkleeEeeeee</button>
         </form>
         <div className="todo-list">
           {todos.map(todo => (
