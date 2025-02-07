e006abf0486b8c62ff23f788c7b87eb4c1e3f072
oktaycabik <oktaycabik2@gmail.com>
2025-02-07 23:11:06 +0300

    Fix EC2 public IP resolution and deployment

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -124,6 +124,7 @@ resource "aws_instance" "backend" {
 # EC2'nin public IP'sini output olarak alma
 output "ec2_public_ip" {
   value = aws_instance.backend.public_ip
+  description = "The public IP address of the EC2 instance"
 }
 
 # Private key'i output olarak alma
