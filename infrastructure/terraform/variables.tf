d8ed453f281422fca433432418b57a26ab221253
oktaycabik <oktaycabik2@gmail.com>
2025-02-08 05:44:12 +0300

    Eksik değişkenleri ekle ve workflow'u güncelle

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -11,12 +11,12 @@ variable "environment" {
 }
 
 variable "iam_user_name" {
-  description = "The IAM user name that will have access to the S3 bucket"
+  description = "S3 bucket erişimi olan IAM kullanıcı adı"
   type        = string
 }
 
 variable "my_public_ip" {
   description = "SSH erişimi için IP adresi"
   type        = string
-  default     = "0.0.0.0/0" # Kendi IP'nizle değiştirin
+  default     = "0.0.0.0/32"
 } 
