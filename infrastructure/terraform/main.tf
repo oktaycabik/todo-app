4aa6842b071c82325ab3c253b8bfc5a9de97c118
oktaycabik <oktaycabik2@gmail.com>
2025-02-08 03:38:54 +0300

    Completely block public access to S3 buckets

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -40,17 +40,19 @@ resource "aws_s3_bucket_policy" "frontend" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "AllowPublicRead"
-        Effect    = "Allow"
+        Sid       = "DenyAllPublicAccess"
+        Effect    = "Deny"
         Principal = "*"
-        Action    = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ]
+        Action    = "s3:*"
         Resource = [
           "${aws_s3_bucket.frontend.arn}",
           "${aws_s3_bucket.frontend.arn}/*"
         ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
       },
       {
         Sid       = "AllowIAMUserAccess"
@@ -143,13 +145,14 @@ resource "aws_cloudfront_distribution" "frontend" {
   origin {
     domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
     origin_id   = "S3Origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
   }
   enabled = true
   default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
     target_origin_id       = "S3Origin"
     viewer_protocol_policy = "redirect-to-https"
 
@@ -163,6 +166,7 @@ resource "aws_cloudfront_distribution" "frontend" {
 
   viewer_certificate {
     cloudfront_default_certificate = true
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   restrictions {
@@ -170,6 +174,14 @@ resource "aws_cloudfront_distribution" "frontend" {
       restriction_type = "none"
     }
   }
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "s3-oac"
+  description                       = "OAC Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 data "aws_caller_identity" "current" {} 
