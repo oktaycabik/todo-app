640a82f7e2a74a8d6fad5a178f1572ff1a9597b1
oktaycabik <oktaycabik2@gmail.com>
2025-02-10 05:05:28 +0300

    dadwqq

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,7 +50,7 @@ function App() {
             placeholder="Yeni görev ekle..."
             className="todo-input"
           />
-          <button type="submit" className="add-button">Ekleeeeeese</button>
+          <button type="submit" className="add-button">Ekleeeeeeese</button>
         </form>
         <div className="todo-list">
           {todos.map(todo => (
